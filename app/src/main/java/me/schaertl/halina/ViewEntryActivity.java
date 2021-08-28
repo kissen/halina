0427eb120862771aa7093982284ea589701ff784
Andreas Schärtl <andreas@schaertl.me>
2021-08-28 10:07:28 +0200

    ViewEntryActivity: set up basic layout

---
+++ app/src/main/java/me/schaertl/halina/ViewEntryActivity.java
@@ -0,0 +1,14 @@
+package me.schaertl.halina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_entry);
+    }
+}
