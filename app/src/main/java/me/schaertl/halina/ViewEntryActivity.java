85c1506bba78e16b3865837baadc871389f56aa2
Andreas Schärtl <andreas@schaertl.me>
2021-08-30 16:10:42 +0200

    ViewEntryActivity: add back button in action bar

--- app/src/main/java/me/schaertl/halina/ViewEntryActivity.java
+++ app/src/main/java/me/schaertl/halina/ViewEntryActivity.java
@@ -1,11 +1,13 @@
 package me.schaertl.halina;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.Spanned;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +27,12 @@ public class ViewEntryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_entry);
 
+        // Configure the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         // Load arguments passed from MainActivity.
         final Bundle arguments = getIntent().getExtras();
         if (arguments != null) {
@@ -38,6 +46,16 @@ public class ViewEntryActivity extends AppCompatActivity {
         // Start looking for the contents.
         final Thread worker = new DefinitionFinder(getApplicationContext());
         worker.start();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @SuppressLint("ShowToast")
