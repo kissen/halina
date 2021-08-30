f9192f4fc723d411d2f711cb97353abf7e54143d
Andreas Schärtl <andreas@schaertl.me>
2021-08-30 22:47:18 +0200

    DefinitionFormatter: output html links

--- app/src/main/java/me/schaertl/halina/support/DefinitionFormatter.java
+++ app/src/main/java/me/schaertl/halina/support/DefinitionFormatter.java
@@ -7,10 +7,16 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import me.schaertl.halina.storage.Definition;
 
 public class DefinitionFormatter {
+    private final static String REGEX = "\\[\\[(.*?)\\]\\]";
+    private final static String SUBSTITUTION = "<a href=\"halina://$1\">$1</a>";
+    private final static Pattern PATTERN = Pattern.compile(REGEX, Pattern.MULTILINE);
+
     private DefinitionFormatter() {
     }
 
@@ -20,18 +26,19 @@ public class DefinitionFormatter {
         buf.append("<ul>\n");
 
         for (final String definition : uniqueElementsIn(boxed.definitions)) {
-            // For now we drop the [[links]] because implementing them is going to be
-            // a topic of its own.
-            final String withoutLinks = definition.replace("[", "").replace("]", "");
-
             buf.append("    <p>");
-            buf.append(withoutLinks);
+            buf.append(htmlifyLinks(definition));
             buf.append("</p>\n");
         }
 
         buf.append("</ul>\n");
 
         return Html.fromHtml(buf.toString(), 0);
+    }
+
+    private static String htmlifyLinks(String definition) {
+        final Matcher matcher = PATTERN.matcher(definition);
+        return matcher.replaceAll(SUBSTITUTION);
     }
 
     private static List<String> uniqueElementsIn(List<String> list) {
