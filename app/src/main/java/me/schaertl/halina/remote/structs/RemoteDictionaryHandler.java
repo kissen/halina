e973745c5edbb054a0b058d6ae1609ea6362ee52
Andreas Schärtl <andreas@schaertl.me>
2022-04-16 13:11:35 +0200

    java: remove unused classes

--- app/src/main/java/me/schaertl/halina/remote/structs/RemoteDictionaryHandler.java
+++
@@ -1,9 +0,0 @@
-package me.schaertl.halina.remote.structs;
-
-public interface RemoteDictionaryHandler {
-    void onNewMeta(RemoteDictionaryMeta meta);
-    void onNewMetaFailed(String errorMessage);
-    void onInstallProgress(String url, Phase phase, Progress progress);
-    void onInstallCompleted(String fileLocation);
-    void onInstallFailed(String errorMessage);
-}
